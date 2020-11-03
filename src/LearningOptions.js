81ba4f2577c1d78fdb577e49f13e4e59a5fa7184
davidecravero <66950139+davidecravero@users.noreply.github.com>
2020-11-03 11:26:49 +0100

    add topics selection

--- src/LearningOptions.js
+++ src/LearningOptions.js
@@ -1,26 +1,26 @@
-// import React from "react";
-// import "./Styles/LearningOptions.css";
+import React from "react";
+import "./Styles/LearningOption.css";
 
-// //topics selection
-// const LearningOptions = (props) => {
-//   const options = [
-//     { text: "topic1", handler: () => {}, id: 1 },
-//     { text: "topic2", handler: () => {}, id: 2 },
-//     { text: "topic3", handler: () => {}, id: 3 },
-//     { text: "topic4", handler: () => {}, id: 4 },
-//   ];
+//topics selection
+const LearningOptions = (props) => {
+  const options = [
+    { text: "topic1", handler: () => {}, id: 1 },
+    { text: "topic2", handler: () => {}, id: 2 },
+    { text: "topic3", handler: () => {}, id: 3 },
+    { text: "topic4", handler: () => {}, id: 4 },
+  ];
 
-//   const optionsMarkup = options.map((option) => (
-//     <button
-//       className="learning-option-button"
-//       key={option.id}
-//       onClick={option.handler}
-//     >
-//       {option.text}
-//     </button>
-//   ));
+  const optionsMarkup = options.map((option) => (
+    <button
+      className="learning-option-button"
+      key={option.id}
+      onClick={option.handler}
+    >
+      {option.text}
+    </button>
+  ));
 
-//   return <div className="learning-options-container">{optionsMarkup}</div>;
-// };
+  return <div className="learning-options-container">{optionsMarkup}</div>;
+};
 
-// export default LearningOptions;
+export default LearningOptions;
--- src/config.js
+++ src/config.js
@@ -1,22 +1,35 @@
 // Config starter code
 import { createChatBotMessage } from "react-chatbot-kit";
+import React from "react";
+import LearningOptions from "./LearningOptions";
+// import LinkList from "LinkList";
 
 const config = {
   initialMessages: [
     createChatBotMessage(
-      "Hi, I'm the chatbot, your personal helper, if you need to get help just type one of this topic: Orders, Shipping, Returns, Refund."
+      "Hi, I'm the chatbot, your personal helper, if you need to get help just type one of this topic: Orders, Shipping, Returns, Refund.",
+      {
+        widget: "learningOptions",
+      }
     ),
   ],
+  widgets: [
+    {
+      widgetName: "learningOptions",
+      widgetFunc: (props) => <LearningOptions {...props} />,
+    },
+  ],
+
   botName: "Chatbot",
-  // Defines an object of custom styles if you want to override styles
+  // Defines an object of custom styles if to override styles
   customStyles: {
     // Overrides the chatbot message styles
     botMessageBox: {
-      backgroundColor: "#ff00c3",
+      backgroundColor: "#ff00e1",
     },
     // Overrides the chat button styles
     chatButton: {
-      backgroundColor: "#ff00c3",
+      backgroundColor: "#ff00e1",
     },
   },
 };
