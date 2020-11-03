59940e33ae5277445799c8d893fa82375325e341
davidecravero <66950139+davidecravero@users.noreply.github.com>
2020-11-03 18:33:37 +0100

    small style changes

--- src/config.js
+++ src/config.js
@@ -3,9 +3,7 @@ import { createChatBotMessage } from "react-chatbot-kit";
 
 let config = {
   initialMessages: [
-    createChatBotMessage(
-      "Hi, I'm the chatbot, your personal helper, please type: I need help, to start the conversation."
-    ),
+    createChatBotMessage("Hi, type help to start the conversation"),
   ],
 };
 
